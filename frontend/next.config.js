16aa0166493bb8a854ed26b7e0069d9bc404f168
Kenji Saito <ken-yo@mbr.nifty.com>
2023-05-06 14:30:59 +0900

    Fix the errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const config =  {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
   cleanDistDir: true,
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    appDir: true,
   },
 };
 
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { ReactNode } from 'react';
+import NoSsr from 'react-no-ssr';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './styles/theme';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </head>
+      <body>
+        <NoSsr>
+          <ThemeProvider theme={theme}>
+            {/* ThemeProvider makes the theme available down the React
+                  tree thanks to React context. */}
+
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </NoSsr>
+      </body>
+    </html>
+  );
+}
