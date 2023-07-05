0dfe2c8978c076c3a451740a43e37bf5ff2cc721
Kenji Saito <1867845+poad@users.noreply.github.com>
2023-07-05 12:27:39 +0900

    Remove the cross-fetch (#7)

--- backend/lambda/index.ts
+++ backend/lambda/index.ts
@@ -1,6 +1,5 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import * as qs from 'qs';
-import fetch from 'cross-fetch';
 import { GraphQLError } from 'graphql';
 import { gql, GraphQLClient } from 'graphql-request';
 
