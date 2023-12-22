a11ed5b3c6de31e226c9433095b38262139e74fb
Kenji Saito <ken-yo@mbr.nifty.com>
2023-12-22 21:18:59 +0900

    Bumps node modules

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -35,7 +35,7 @@ export class BackendStack extends cdk.Stack {
 
     const fn = new nodejs.NodejsFunction(this, 'LambdaFunction', {
       functionName,
-      runtime: lambda.Runtime.NODEJS_18_X,
+      runtime: lambda.Runtime.NODEJS_20_X,
       architecture: lambda.Architecture.ARM_64,
       entry: './lambda/index.ts',
       retryAttempts: 0,
